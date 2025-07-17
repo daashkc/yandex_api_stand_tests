2a4a5f20ce6a15fb230e72f769e984a98cc6975e
Dasha Paleeva <daashkc@yandex.ru>
2025-07-17 21:44:55 +0700

    1 comit

---
+++ sender_stand_request.py
@@ -0,0 +1,31 @@
+import configuration
+import requests
+import data
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+response = post_new_user(data.user_body)
+
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+
+
+response = post_products_kits(data.products_ids)
+
+
+def get_users_table():
+    # Составление полного URL пути к данным таблицы пользователей
+    # путем конкатенации базового URL сервиса и конечной точки таблицы пользователей
+    # Возвращает объект ответа от сервера
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+response = get_users_table()
